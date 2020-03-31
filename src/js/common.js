659785def798091b0e20accc7f27d3b333dfd7b4
youngmin <g369369g@enjoyworks.co.kr>
2020-03-31 18:34:22 +0900

    2020-03-31 / 완료본

--- src/js/common.js
+++ src/js/common.js
@@ -14,9 +14,9 @@ $(document).ready(function(){
 
     $(window).scroll(function(){
         if($(window).scrollTop() > 0) {
-            $('.header').addClass('scrolled');
+            $('header.fixed').addClass('scrolled');
         } else {
-            $('.header').removeClass('scrolled');
+            $('header.fixed').removeClass('scrolled');
         }
     });
 
